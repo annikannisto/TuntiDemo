782f5d5aef473d5201de97931635903f2eaae524
annikannisto <anni.kannisto@metropolia.fi>
2024-09-27 09:48:09 +0300

    koodia

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Calculator {
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Done docker");
+    }
 }
